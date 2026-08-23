6ae738e8da120c4bcc9b9f43dd16f517605937f3
k3ijay <274225776+k3ijay@users.noreply.github.com>
2026-08-23 22:13:00 +0900

    first implementation

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("header#header-container").load("asset/header.html");
+    $("nav.navbar").load("asset/nav.html");
+});
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide(".splide").mount(window.splide.Extensions);
+    
+});
+const splide = new Splide(".splide", {
+    autoplay: true,
+    type: "loop",
+    pauseOnHover: false, 
+    pauseOnFocus: false,
+    interval: 4000, // auto play interval
+    speed: 1000, // slider transition time
+}).mount();
