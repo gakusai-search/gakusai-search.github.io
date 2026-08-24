4efab6adb5b2685365a65907917e86f45fa1188c
k3ijay <274225776+k3ijay@users.noreply.github.com>
2026-08-24 14:19:48 +0900

    refactor: Move footer inline script to main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $("header#header-container").load("asset/header.html");
     $("nav.navbar").load("asset/nav.html");
+    $(document).ready(function () {
+        $("footer").load("../../asset/footer.html");
+    });
 });
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide(".splide").mount(window.splide.Extensions);
